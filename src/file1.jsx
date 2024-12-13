b05749129e05bab974e6dd8967fc5c6ef30f4a0f
Chirag-parakh <cparakh53@gmail.com>
2024-12-13 10:15:03 +0530

    Revert "added 2 files"
    This reverts commit 209f79bda2c441c48939b30877394f997dfefa68.

--- src/file1.jsx
+++
@@ -1,6 +0,0 @@
-
-export default function File1() {
-    return (
-        <div>file1</div>
-    )
-}
--- src/file2.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function file2() {
-    return (
-        <div>file2</div>
-    )
-}
